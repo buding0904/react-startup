bda29e32337a396cf90c811af1d5269f62df947f
zhengmao <zhengmao0613@gmail.com>
2026-01-09 14:06:33 +0800

    chore: remove inject uno css color variables to css

--- uno.config.ts
+++ uno.config.ts
@@ -3,24 +3,24 @@ import { defineConfig } from 'unocss'
 export default defineConfig({
   shortcuts: {},
   preflights: [
-    {
-      getCSS: ({ theme }) => {
-        const generateColorVariables = (obj, prefix = '') => {
-          let cssVariables = ''
-          for (const [key, value] of Object.entries(obj)) {
-            if (key === 'DEFAULT') {
-              cssVariables += `  --${prefix}: ${value};\n`
-            } else if (typeof value === 'string') {
-              cssVariables += `  --${prefix}-${key}: ${value};\n`
-            } else if (typeof value === 'object') {
-              cssVariables += generateColorVariables(value, `${prefix}${key}`)
-            }
-          }
-          return cssVariables
-        }
+    // {
+    //   getCSS: ({ theme }) => {
+    //     const generateColorVariables = (obj: any, prefix = '') => {
+    //       let cssVariables = ''
+    //       for (const [key, value] of Object.entries(obj)) {
+    //         if (key === 'DEFAULT') {
+    //           cssVariables += `  --${prefix}: ${value};\n`
+    //         } else if (typeof value === 'string') {
+    //           cssVariables += `  --${prefix}-${key}: ${value};\n`
+    //         } else if (typeof value === 'object') {
+    //           cssVariables += generateColorVariables(value, `${prefix}${key}`)
+    //         }
+    //       }
+    //       return cssVariables
+    //     }
 
-        return `:root {\n${generateColorVariables(theme.colors)}}`
-      },
-    },
+    //     return `:root {\n${generateColorVariables(theme.colors)}}`
+    //   },
+    // },
   ],
 })
