3b4de2139dd56f68f7d0a2bbd2cb7b770f333c80
zhengmao <zhengmao0613@gmail.com>
2026-03-19 13:21:07 +0800

    update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import ctx, { useInitialAppContext } from '@/context/app'
+
+const CompA: FC = () => {
+  const appContext = useContext(ctx)
+  console.log('CompA rerender')
+
+  return (
+    <div>
+      CompA: {appContext.count}
+      <button onClick={() => appContext.setCount(prev => prev + 1)}>+</button>
+    </div>
+  )
+}
+
+const CompB: FC = () => {
+  const appContext = useContext(ctx)
+  console.log('CompB rerender')
+  return (
+    <div>
+      CompB: {appContext.countB}
+      <button onClick={() => appContext.setCountB(prev => prev + 1)}>+</button>
+    </div>
+  )
+}
 
 const App: FC = () => {
   const initialContext = useInitialAppContext()
@@ -7,6 +30,8 @@ const App: FC = () => {
   return (
     <ctx.Provider value={initialContext}>
       <div className="text-red">App</div>
+      <CompA />
+      <CompB />
     </ctx.Provider>
   )
 }
--- src/context/app.ts
+++ src/context/app.ts
@@ -3,14 +3,17 @@ import { createContext, useState } from 'react'
 interface AppContext {
   count: number
   setCount: ContextSetter<number>
+  countB: number
+  setCountB: ContextSetter<number>
 }
 
 const ctx = createContext<AppContext>({} as AppContext)
 
 export function useInitialAppContext(): AppContext {
   const [count, setCount] = useState(0)
+  const [countB, setCountB] = useState(0)
 
-  return { count, setCount }
+  return { count, setCount, countB, setCountB }
 }
 
 export default ctx
